ac1545eab8b6d061980338f6a291d1eaf2e5343f
Tati <ridarella13@gmail.com>
2022-12-29 12:56:14 +0300

    added sout

---
+++ src/test/java/NewTest.java
@@ -0,0 +1,9 @@
+import org.junit.jupiter.api.Test;
+import org.testng.asserts.Assertion;
+
+public class NewTest {
+    @Test
+    void newTest() {
+        System.out.println("Hello, world");
+        Assertion.assertTrue(3 > 2);
+    }
